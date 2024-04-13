c9b58b84a1c99883d9837faaaeedbe343ce00628
DonSanoj <sanojaminda@gmail.com>
2024-04-13 13:00:29 +0530

    update

---
+++ bookTable.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta name="description" content="Cafe Express, cafe express">
+    <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="/assets/css/bookTable.css">
+    <link rel="stylesheet" href="/assets/css/header.css">
+    <link rel="stylesheet" href="/assets/css/footer.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+
+    <title>Cafe | Book a table</title>
+</head>
+
+<body>
+
+    <?php include("./includes/header.php") ?>
+
+    <div class="heading">
+        <h3><i class='fas fa-circle'></i></h3>
+        <h1>Book a table by clicking on the table</h1>
+    </div>
+
+    <div class="tables">
+        <div class="table-row-1">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+        </div>
+        <div class="table-row-2">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+        </div>
+        <div class="table-row-3">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+            <img src="/assets/image/conference-room (1).png" alt="table">
+        </div>
+    </div>
+
+    <?php include("./includes/footer.php") ?>
+
+</body>
+
+</html>
